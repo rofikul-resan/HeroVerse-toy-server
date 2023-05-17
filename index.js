5f4307a3f46cacc0d82e09e0790786e41ea2862f
rofikul-resan <resan6231@gmail.com>
2023-05-18 00:59:03 +0600

    file set up

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hay buddy i am running");
+});
+
+app.listen(port);
